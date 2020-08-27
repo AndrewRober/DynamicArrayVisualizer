00c62b30d6355ea6b468db7453eaf1df13e097d6
andrew rober <andrew_rober@hotmail.com>
2020-08-27 14:02:42 +0200

    Add project files.

---
+++ DynamicArrayVisualizer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DynamicArrayVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 327670;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1106, 81);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1124, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(344, 522);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(362, 99);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(837, 522);
+            this.treeView1.TabIndex = 3;
+            // 
+            // Id
+            // 
+            this.Id.FillWeight = 50F;
+            this.Id.Frozen = true;
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Id.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Id.Width = 50;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.FillWeight = 300F;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Value.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 633);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "DynamicArray Visualizer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ DynamicArrayVisualizer/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicArrayVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public static readonly CultureInfo EngCulture = new CultureInfo("en-US", false);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            var fields = textBox1.Text.Split(Convert.ToChar(254).ToString(EngCulture).First());
+            for (int i = 0; i < fields.Count(); i++)
+                dataGridView1.Rows.Add(i + 1, fields[i]);
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            var field = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            var values = field.Split(Convert.ToChar(253).ToString(EngCulture).First());
+            foreach (var val in values)
+            {
+                if (string.IsNullOrEmpty(val)) continue;
+                var valueNode = new TreeNode(val);
+                var subvalues = val.Split(Convert.ToChar(252).ToString(EngCulture).First());
+                if (subvalues.Count() > 1)
+                    foreach (var subval in subvalues)
+                    {
+                        if (string.IsNullOrEmpty(subval)) continue;
+                        var subvalNode = new TreeNode(subval);
+                        var TextValues = subval.Split(Convert.ToChar(251).ToString(EngCulture).First());
+                        if (TextValues.Count() > 1)
+                            foreach (var textval in TextValues)
+                            {
+                                if (string.IsNullOrEmpty(textval)) continue;
+                                var textvalNode = new TreeNode(textval);
+                                //var subTextValues = textval.Split(Convert.ToChar(250).ToString(EngCulture).First());
+                                //if (subTextValues.Count() > 1)
+                                //    textvalNode.Nodes.Add(string.Join(", ", subTextValues));
+                                    //foreach (var stv in subTextValues)
+                                    //{
+                                    //    if (string.IsNullOrEmpty(stv)) continue;
+                                    //    textvalNode.Nodes.Add(stv);
+                                    //}
+                                subvalNode.Nodes.Add(textvalNode);
+                            }
+                        valueNode.Nodes.Add(subvalNode);
+                    }
+                treeView1.Nodes.Add(valueNode);
+            }
+            treeView1.ExpandAll();
+        }
+    }
+}
